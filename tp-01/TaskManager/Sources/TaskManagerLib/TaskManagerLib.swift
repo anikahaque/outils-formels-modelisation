87b4bc4ea92ddf0bb967886955911d9695bd6478
anikahaque <anika.haque@etu.unige.ch>
2017-10-26 21:11:31 +0200

    Tp 2 final

---
+++ tp-01/TaskManager/Sources/TaskManagerLib/TaskManagerLib.swift
@@ -0,0 +1,74 @@
+
+
+import PetriKit
+
+public func createTaskManager() -> PTNet {
+    // Places
+    let taskPool    = PTPlace(named: "taskPool")
+    let processPool = PTPlace(named: "processPool")
+    let inProgress  = PTPlace(named: "inProgress")
+
+    // Transitions
+    let create      = PTTransition(
+        named          : "create",
+        preconditions  : [],
+        postconditions : [PTArc(place: taskPool)])
+    let spawn       = PTTransition(
+        named          : "spawn",
+        preconditions  : [],
+        postconditions : [PTArc(place: processPool)])
+    let success     = PTTransition(
+        named          : "success",
+        preconditions  : [PTArc(place: taskPool), PTArc(place: inProgress)],
+        postconditions : [])
+    let exec       = PTTransition(
+        named          : "exec",
+        preconditions  : [PTArc(place: taskPool), PTArc(place: processPool)],
+        postconditions : [PTArc(place: taskPool), PTArc(place: inProgress)])
+    let fail        = PTTransition(
+        named          : "fail",
+        preconditions  : [PTArc(place: inProgress)],
+        postconditions : [])
+
+    // P/T-net
+    return PTNet(
+        places: [taskPool, processPool, inProgress],
+        transitions: [create, spawn, success, exec, fail])
+}
+
+
+public func createCorrectTaskManager() -> PTNet {
+    // Places
+    let taskPool    = PTPlace(named: "taskPool")
+    let processPool = PTPlace(named: "processPool")
+    let inProgress  = PTPlace(named: "inProgress")
+    let correcteur  = PTPlace(named: "correcteur")
+    //On ajoute une nouvelle place pour corriger le problème,elle sert à associer chaque tâche à un processeur.
+    // Elle a comme précondition la transition exec et comme postconditions les transtions create et fail
+    // Transitions
+    let create      = PTTransition(
+        named          : "create",
+        preconditions  : [],
+        postconditions : [PTArc(place: taskPool), PTArc(place: correcteur)])
+    let spawn       = PTTransition(
+        named          : "spawn",
+        preconditions  : [],
+        postconditions : [PTArc(place: processPool)])
+    let success     = PTTransition(
+        named          : "success",
+        preconditions  : [PTArc(place: taskPool), PTArc(place: inProgress)],
+        postconditions : [])
+    let exec       = PTTransition(
+        named          : "exec",
+        preconditions  : [PTArc(place: taskPool), PTArc(place: processPool), PTArc(place: correcteur)],
+        postconditions : [PTArc(place: taskPool), PTArc(place: inProgress)])
+    let fail        = PTTransition(
+        named          : "fail",
+        preconditions  : [PTArc(place: inProgress)],
+        postconditions : [PTArc(place: correcteur)])
+
+    // P/T-net
+    return PTNet(
+        places: [taskPool, processPool, inProgress, correcteur],
+        transitions: [create, spawn, success, exec, fail])
+}
--- tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
+++ tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
@@ -1,3 +1,4 @@
+
 import PetriKit
 
 public class MarkingGraph {
@@ -16,28 +17,100 @@ public extension PTNet {
 
     public func markingGraph(from marking: PTMarking) -> MarkingGraph? {
 
-        let m0:MarkingGraph = MarkingGraph(marking: marking)
-        var seen :[MarkingGraph]=[m0]
-        var toVisit:[MarkingGraph]=[m0]
-        while let current = toVisit.popLast(){
-          seen.append(current)
-          for tr in transitions{
-              if let firedtr = tr.fire(from: current.marking){
-                if let visited = seen.first(where: {$0.marking == firedtr}){
-                     current.successors[tr] = visited
-}else{
-let successor:MarkingGraph = MarkingGraph(marking: firedtr)
-        if !seen.contains{$0 === successor}{
-             current.successors[tr] = successor
-            toVisit.append(successor)
 
-          }
-          }
-          }
+        let Noeud = MarkingGraph(marking: marking)
+        var Avoir = [MarkingGraph]()
+        var Vu = [MarkingGraph]()
+
+        Avoir.append(Noeud)
+
+        while Avoir.count != 0 {
+            let current = Avoir.removeFirst()
+            Vu.append(current)
+            transitions.forEach { tr in
+              if let mark = tr.fire(from: current.marking) {
+                        if let dejavu = Vu.first(where: { $0.marking == mark }) {
+                            current.successors[tr] = dejavu
+                        } else {
+                            let successor = MarkingGraph(marking: mark)
+                            current.successors[tr] = successor
+                            if (!Avoir.contains(where: { $0.marking == successor.marking})) {
+                                Avoir.append(successor)
+                            }
+                    }
+                }
+            }
         }
+
+
+        return Noeud
+    }
+
+    public func count (mark: MarkingGraph) -> Int{
+      var vu = [MarkingGraph]()
+      var Avoir = [MarkingGraph]()
+
+      Avoir.append(mark)
+      while let current = Avoir.popLast() {
+        vu.append(current)
+        for(_, successor) in current.successors{
+          if !vu.contains(where: {$0 === successor}) && !Avoir.contains(where: {$0 === successor}){
+              Avoir.append(successor)
+            }
+          }
       }
-          print("\(seen)")
-          return m0
+
+      return vu.count
+    }
+
+    public func Smokers (mark: MarkingGraph) -> Bool {
+      var vu = [MarkingGraph]()
+      var Avoir = [MarkingGraph]()
+
+      Avoir.append(mark)
+      while let current = Avoir.popLast() {
+        vu.append(current)
+        var fumeur = 0;
+        for (key, value) in current.marking {
+            if (key.name == "s1" || key.name == "s2" || key.name == "s3"){
+               fumeur += Int(value)
+            }
         }
+        if (fumeur > 1) {
+          print (current.marking)
+          return true
+        }
+        for(_, successor) in current.successors{
+          if !vu.contains(where: {$0 === successor}) && !Avoir.contains(where: {$0 === successor}){
+              Avoir.append(successor)
+            }
+          }
+      }
+      return false
+    }
+
+    public func Multiple (mark: MarkingGraph) -> Bool {
+      var vu = [MarkingGraph]()
+      var Avoir = [MarkingGraph]()
+
+      Avoir.append(mark)
+      while let current = Avoir.popLast() {
+        vu.append(current)
+        for (key, value) in current.marking {
+            if (key.name == "p" || key.name == "t" || key.name == "m"){
+               if(value > 1){
+               print(current.marking)
+                 return true
+               }
+            }
+        }
+        for(_, successor) in current.successors{
+          if !vu.contains(where: {$0 === successor}) && !Avoir.contains(where: {$0 === successor}){
+              Avoir.append(successor)
+            }
+          }
+      }
+      return false
+    }
 
 }
