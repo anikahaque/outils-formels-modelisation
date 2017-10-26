e5babb5f12113f7aa83d0a6fadf001c1fb164c62
anikahaque <anika.haque@etu.unige.ch>
2017-10-26 15:11:01 +0200

    TP 2 essai

--- tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
+++ tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
@@ -15,8 +15,29 @@ public class MarkingGraph {
 public extension PTNet {
 
     public func markingGraph(from marking: PTMarking) -> MarkingGraph? {
-        // Write here the implementation of the marking graph generation.
-        return nil
-    }
+
+        let m0:MarkingGraph = MarkingGraph(marking: marking)
+        var seen :[MarkingGraph]=[m0]
+        var toVisit:[MarkingGraph]=[m0]
+        while let current = toVisit.popLast(){
+          seen.append(current)
+          for tr in transitions{
+              if let firedtr = tr.fire(from: current.marking){
+                if let visited = seen.first(where: {$0.marking == firedtr}){
+                     current.successors[tr] = visited
+}else{
+let successor:MarkingGraph = MarkingGraph(marking: firedtr)
+        if !seen.contains{$0 === successor}{
+             current.successors[tr] = successor
+            toVisit.append(successor)
+
+          }
+          }
+          }
+        }
+      }
+          print("\(seen)")
+          return m0
+        }
 
 }
